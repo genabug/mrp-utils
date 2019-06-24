6ef009086228f5f824a846f75ef1a0f0ffaa82bf
gena bug <archaerolog@gmail.com>
2019-06-24 18:13:27 +0300

    remove sources from INTERFACE library

--- utils/utils/QState.cpp
+++
@@ -1,7 +0,0 @@
-/*!
-  \file QState.cpp
-  \author gennadiy (gennadiy3.14@gmail.com)
-  \brief Documentation of class to represent vector of quantities (state).
-*/
-
-// someday... with a lot of examples. I promise!
