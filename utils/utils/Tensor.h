9716aae7ef7b91f45e83726ee172ee3a8b240b52
gena bug <archaerolog@gmail.com>
2019-07-15 14:05:08 +0300

    moved compile-time tests of vector and tensor to the definition files

--- utils/utils/Tensor.h
+++ utils/utils/Tensor.h
@@ -550,6 +550,114 @@ template<size_t N, class T> template<size_t I, class, class> // full init
 }
 
 /*---------------------------------------------------------------------------------------*/
+/*--------------------------------------- tests -----------------------------------------*/
+/*---------------------------------------------------------------------------------------*/
+
+namespace tensor_tests
+{
+  using T2d = Tensor<2>;
+  using T2i = Tensor<2, int>;
+  using T3i = Tensor<3, int>;
+  using V2i = Vector<2, int>;
+  using V3i = Vector<3, int>;
+
+  // init
+  constexpr T2i td, Z(0), E(1, 1), t1(1, 2), t2(3, 4, 5, 6);
+
+  // access
+  static_assert((Z[0][0] == 0) && (Z[0][1] == 0) &&
+                (Z[1][0] == 0) && (Z[1][1] == 0), "single init failed");
+
+  static_assert((t1[0][0] == 1) && (t1[0][1] == 0) &&
+                (t1[1][0] == 0) && (t1[1][1] == 2), "diagonal init failed");
+
+  static_assert((t2[0][0] == 3) && (t2[0][1] == 4) &&
+                (t2[1][0] == 5) && (t2[1][1] == 6), "full init failed");
+
+  // conversion
+  constexpr T2d d(1., 2., 3., 4.);
+  constexpr T2i m = T2i(d);
+  static_assert((m[0][0] == 1) && (m[0][1] == 2) &&
+                (m[1][0] == 3) && (m[1][1] == 4), "conversion d -> i failed");
+
+  // ops
+  static_assert(td == td, "t == t failed");
+  static_assert(td != t1, "t != t failed");
+
+  constexpr T2i t1_plus_t1(2, 4), t1_plus_t2(4, 4, 5, 8);
+  static_assert(Z + Z == Z, "Z + Z failed");
+  static_assert(Z + t1 == t1, "Z + t failed");
+  static_assert(t1 + Z == t1, "t + Z failed");
+  static_assert(t1 + t1 == t1_plus_t1, "t + t failed");
+  static_assert(t1 + t2 == t1_plus_t2, "t1 + t2 failed");
+
+  constexpr T2i t2_minus_t1(2, 4, 5, 4);
+  static_assert(t2 - t2 == Z, "t - t failed");
+  static_assert(t2 - Z == t2, "t - Z failed");
+  static_assert(Z - t2 == -t2, "Z - t failed");
+  static_assert(t2 - t1 == t2_minus_t1, "t2 - t1 failed");
+
+  static_assert(Z * t2 == Z, "Z * t failed");
+  static_assert(t2 * Z == Z, "t * Z failed");
+
+  static_assert(E * t2 == t2, "E * t failed");
+  static_assert(t2 * E == t2, "t * E failed");
+
+  constexpr T2i t3(3, 4, 10, 12), t4(3, 8, 5, 12);
+  static_assert(t1 * t2 == t3, "t * t failed");
+  static_assert(t2 * t1 == t4, "t * t failed");
+
+  constexpr T2i t5(2), t6(1);
+  static_assert(t6 * 2 == t5, "t * a failed");
+  static_assert(t5 / 2 == t6, "t / a failed");
+
+  // vector ops
+  constexpr V2i z(0), v(2, 3);
+  static_assert(t2 * z == z, "t * z failed");
+  static_assert(z * t2 == z, "z * t failed");
+  static_assert(E * v == v, "E * v failed");
+  static_assert(v * E == v, "v * E failed");
+
+  constexpr V2i v_mult_t(21, 26), t_mult_v(18, 28);
+  static_assert(v * t2 == v_mult_t, "v * t failed");
+  static_assert(t2 * v == t_mult_v, "t * v failed");
+
+  constexpr T2i v_diad_v(4, 6, 6, 9);
+  static_assert((v ^ v) == v_diad_v, "v ^ v failed");
+
+  // 2D vector ops
+  constexpr V2i t2_cross_v(1, 3);
+  static_assert(Z % v == z, "Z % v failed in 2D");
+  static_assert(t2 % z == z, "T % z failed in 2D");
+  static_assert(t2 % v == t2_cross_v, "t % v failed in 2D");
+  static_assert(v % t3 == ~t3 % (-v), "v % t failed in 2D");
+
+  // 3D vector ops
+  constexpr T3i ZZ(0), T(1, 2, 3, 4, 5, 6, 7, 8, 9);
+  constexpr V3i v1(1, 2, 3), v2(4, 5, 6), zz(0);
+  constexpr T3i T_cross_v1(0, 0, 0, 3, -6, 3, 6, -12, 6);
+  static_assert(~v1 * v2 == v1 % v2, "~v failed in 3D");
+  static_assert(ZZ % v1 == ZZ, "Z % v failed in 3D");
+  static_assert(T % zz == ZZ, "t % z failed in 3D");
+  static_assert(T % v1 == T_cross_v1, "t % v failed in 3D");
+  static_assert(v1 % T == ~(~T % (-v1)), "v % t failed in 3D");
+
+  // other methods
+  static_assert(E.det() == 1, "|E| failed");
+  static_assert(Z.det() == 0, "|Z| failed ");
+  static_assert(t1.det() == 2, "|t| failed");
+  static_assert(t1.trace() == 3, "tr(t) failed");
+  static_assert(E.invert() == E, "E^-1 failed");
+  static_assert(E == ~E, "E^T failed");
+
+  constexpr T2i t(2, 1, 3, 2), tT(2, 3, 1, 2), t_(2, -1, -3, 2);
+  static_assert(~t == tT, "t^T failed");
+  static_assert(t.invert() == t_, "t^-1 failed");
+  static_assert(t.invert() * t == E, "t^-1 * t failed");
+  static_assert(t * t.invert() == E, "t * t^-1 failed");
+}
+
+/*---------------------------------------------------------------------------------------*/
 /*----------------------------------- documentation -------------------------------------*/
 /*---------------------------------------------------------------------------------------*/
 
