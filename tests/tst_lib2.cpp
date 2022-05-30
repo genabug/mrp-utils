820f8e080d6b3fc897af8f6a3708a3c1f3c394cc
gena bug <archaerolog@gmail.com>
2022-05-30 15:53:02 +0200

    added test file

---
+++ tests/tst_lib2.cpp
@@ -0,0 +1,11 @@
+#include "lib2/derived21.hpp"
+#include "lib2/derived22.hpp"
+#include "doctest.h"
+#include <string>
+#include <vector>
+
+TEST_CASE("lib2")
+{
+  Derived21 d21;
+  CHECK_NOTHROW(d21.addr());
+}
