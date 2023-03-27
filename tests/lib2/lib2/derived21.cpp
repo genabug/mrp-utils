12d686ac0318e459b9f7fb68e1b5e636010b6231
gbug <archaerolog@gmail.com>
2023-03-27 22:57:56 +0200

    made both libraries shared, fixed paths

--- tests/lib2/lib2/derived21.cpp
+++ tests/lib2/lib2/derived21.cpp
@@ -1,6 +1,6 @@
 #include "derived21.hpp"
 
-#include "../lib1/base1.hpp"
+#include "lib1/base1.hpp"
 
 const std::string Derived21::name = "Derived21";
 ObjectsFactory<Base2F> reg_d21("Derived21", []() { return new Derived21; });
--- tests/lib2/lib2/derived22.cpp
+++ tests/lib2/lib2/derived22.cpp
@@ -1,5 +1,5 @@
 #include "derived22.hpp"
-#include "../lib1/base1.hpp"
+#include "lib1/base1.hpp"
 
 const std::string Derived22::name = "Derived22";
 ObjectsFactory<Base2F> reg_d22("Derived22", []() { return new Derived22; });
